a9262363a4360d12d31ea65b5864a86032ce71f0
Link <linkdoyle@163.com>
2021-12-19 21:06:08 +0800

    func: implement interface `ExecutorService` for `Endpoints`.

--- pkg/endpoint/executor.go
+++ pkg/endpoint/executor.go
@@ -2,8 +2,10 @@ package endpoint
 
 import (
 	"context"
+
 	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/kit/log"
+
 	pb "qtask/api/proto/v1"
 	"qtask/pkg/model"
 	"qtask/pkg/service"
@@ -57,4 +59,37 @@ func MakeRunTask(s service.ExecutorService) endpoint.Endpoint {
 		err := s.RunTask(context, taskDetail, taskDetailUpdated)
 		return nil, err
 	}
+}
+
+func (e *Endpoints) Health(ctx context.Context) (bool, error) {
+	resp, err := e.HealthEndpoint(ctx, nil)
+	if err != nil {
+		return false, err
+	}
+	response := resp.(HealthCheckResponse)
+	return response.Healthy, nil
+}
+
+func (e *Endpoints) RunTask(ctx context.Context, taskDetail *model.TaskDetail, taskDetailUpdated chan model.TaskDetail) error {
+	panic("implement me")
+}
+
+func (e *Endpoints) WatchTasks(ctx context.Context) (service.TaskMonitor, error) {
+	panic("implement me")
+}
+
+func (e *Endpoints) KillTask(ctx context.Context, id model.TaskId) error {
+	panic("implement me")
+}
+
+func (e *Endpoints) StopTask(ctx context.Context, id model.TaskId) error {
+	panic("implement me")
+}
+
+func (e *Endpoints) PauseTask(ctx context.Context, id model.TaskId) error {
+	panic("implement me")
+}
+
+func (e *Endpoints) ResumeTask(ctx context.Context, id model.TaskId) error {
+	panic("implement me")
 }
